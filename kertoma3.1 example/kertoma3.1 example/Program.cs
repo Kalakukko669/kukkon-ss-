00614d210c5a3a73f5bc644dc3db42100139f838
Kalakukko669 <aatu.savolainen@student.saimia.fi>
2019-09-18 15:00:42 +0300

    making commit

---
+++ kertoma3.1 example/kertoma3.1 example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kertoma3._1_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
