d429492ec57042f22eebd959607d41614216e62b
Kalakukko669 <aatu.savolainen@student.saimia.fi>
2019-09-26 14:17:31 +0300

    IfTask1 complete Program.cs

--- IfTask1/IfTask1/Program.cs
+++ IfTask1/IfTask1/Program.cs
@@ -6,7 +6,27 @@ namespace IfTask1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma selvittää käyttäjän syöttämästäl luvusta, onko se posotiivinen vai negatiivinen!");
+            Console.Write("Syötä kokonaisluku: ");
+            int userInputNumber = int.Parse(Console.ReadLine());
+
+            if (userInputNumber == 0)
+            {
+                Console.WriteLine($"Luku {userInputNumber} on nolla!");
+            }
+            else
+            {
+                if (userInputNumber > 0)
+                {
+                    Console.WriteLine($"Luku {userInputNumber} on positiivinen.");
+                }
+                else
+                {
+                    Console.WriteLine($"Luku {userInputNumber} on negatiivinen.");
+                }
+
+            }
+                                  
         }
     }
 }
