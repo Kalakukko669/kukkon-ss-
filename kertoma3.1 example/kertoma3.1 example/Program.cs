60529d2a66b98fc52ed8354b2a78f624488eaae0
Kalakukko669 <aatu.savolainen@student.saimia.fi>
2019-09-26 12:50:30 +0300

    ReadmWriteexample

--- kertoma3.1 example/kertoma3.1 example/Program.cs
+++ kertoma3.1 example/kertoma3.1 example/Program.cs
@@ -6,7 +6,32 @@ namespace kertoma3._1_example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = 0;
+            int fact = 1;
+
+            // print message
+            Console.WriteLine("Keroman laskenta");
+            Console.Write("Minkä luvun kertoma lasketaan?");
+            n = int.Parse(Console.ReadLine());
+
+            if(n < 0)
+            {
+                Console.WriteLine("Virheellinen syöte!");
+            }
+            else
+            {
+                //silmukka, jossa lasketaan kertoman arvo
+                while(n > 0)
+                {
+                    fact = fact * n; // lasketaan kertoma joka kierroksella
+                    n = n - 1; // vähennetään arvoa yhdellä
+
+                }
+                // Print answer screen
+                Console.WriteLine("Vastaus" + fact);
+            }
+
+
         }
     }
 }
