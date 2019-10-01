0feaa44536bfd90d27682fd306b370399cfb171e
Kalakukko669 <aatu.savolainen@student.saimia.fi>
2019-10-01 11:22:42 +0300

    if task3 started

---
+++ IfTask1/IfTask1.3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace IfTask1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma lukee syötteesi ja selvittää arvon!");
+            Console.Write("Syötä kokonaisluku:");
+            int userInputNumber = int.Parse(Console.ReadLine());
+
+            if (userInputNumber <= 0)
+            {
+               if(userInputNumber <= 0)
+               {
+                    Console.WriteLine($"Numero on {userInputNumber} is zero");
+               }
+               else
+               {
+                    if (userInputNumber % 2 == 0)
+                        Console.WriteLine($"Numero on{userInputNumber} is negative");
+                    {
+                        if (userInputNumber < 0)
+                            
+                        {
+                            Console.WriteLine($"Numero on{userInputNumber} is negative even");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Numero on{userInputNumber} is negative odd");
+                        }
+                    }
+               }
+
+            }
+            else
+            {
+                if (userInputNumber % 2 == 0)
+                    Console.WriteLine($"Numero on{userInputNumber} is positive even");
+                {
+                    if (userInputNumber < 0)
+                    {
+                        Console.WriteLine($"Numero on{userInputNumber} is positive odd");
+                    }
+                }
+            }
+
+        }
+    }
+}
--- IfTask1/IfTask2/Program.cs
+++ IfTask1/IfTask2/Program.cs
@@ -6,7 +6,18 @@ namespace IfTask2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ohjelma selvittää onko luku parillinen vai pariton");
+            Console.WriteLine("Ohjelma selvittää onko luku parillinen vai pariton!");
+            Console.Write("Syötä kokonaisluku: ");
+            int UserInputNumber = int.Parse(Console.ReadLine());
+            
+            if (UserInputNumber % 2 == 0)
+            {
+                Console.WriteLine($"Numero {UserInputNumber} on parillinen");
+            }
+            else
+            {
+                Console.WriteLine($"Numero{UserInputNumber} on pariton");
+            }
         }
     }
 }
