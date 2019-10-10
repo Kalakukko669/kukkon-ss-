a7a33bccf11e33a0547e16417bf9b9c32e500dd6
Kalakukko669 <aatu.savolainen@student.saimia.fi>
2019-10-10 08:23:49 +0300

    If task 1.4 done

---
+++ IfTask1/If task4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace If_task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kolmen luvun lajittelu!");
+            //Tietojen kysely
+            Console.WriteLine("Syötä 1.luku");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Syötä 2.luku");
+            int d = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Syötä 3.luku");
+            int a = int.Parse(Console.ReadLine());
+            //Logiikka jolla ongelma ratkaistaan
+            if (x < d)
+            {
+                if (d < a)
+                {
+                    Console.WriteLine($" {x}, {d} , {a} ");
+                }
+                else
+                {
+                    if (x > a)
+                    {
+                        Console.WriteLine($" {a} , {x} , {d}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($" {x} , {a} , {d} ");
+                    }
+                }
+
+            }
+            else if (x < a)
+            {
+                Console.WriteLine($" {d} , {x} , {a} ");
+            }
+            else if (d > a)
+            {
+                Console.WriteLine($" {d} ,{a} , {x} ");
+            }
+            else
+            {
+                Console.WriteLine($" {a} , {d} ,{x}");
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
--- IfTask1/Iftask4/Program.cs
+++ IfTask1/Iftask4/Program.cs
@@ -2,26 +2,30 @@
 
 namespace Iftask4
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+
+}
+
+    
+        static void Main(string[] args
             Console.WriteLine("Kolmen luvun lajittelu!");
             // Tietojen kysely
             Console.WriteLine("Syötä 1.luku");
             int x = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Syötä 2.luku");
-            int x = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Syötä 3.luku");
-            int x = int.Parse(Console.ReadLine());
+            int a = int.Parse(Console.ReadLine());
 
             // Logiikka, jolla ratkaistaan ongelma
-            if(x<y)
+            if(x<d)
             {
-
+               
             }
-        }
-    }
-}
+            else if(x<a)
+              
+        
+
+                
+ 
