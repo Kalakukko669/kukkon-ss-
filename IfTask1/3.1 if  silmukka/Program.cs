f49b2e1d094871dc90649e98926219a638cff55b
Kalakukko669 <aatu.savolainen@student.saimia.fi>
2019-10-15 11:11:28 +0300

    made new change project 31

--- IfTask1/3.1 if  silmukka/Program.cs
+++ IfTask1/3.1 if  silmukka/Program.cs
@@ -7,7 +7,12 @@ namespace _3._1_if__silmukka
         static void Main(string[] args)
         {
             Console.WriteLine("Ohjelma tulostaa N! kertoman arvon!");
-            Console.WriteLine("Syöt 1.luku");
+            Console.WriteLine("Syötä 1.luku");
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine("Syötä 2.luku");
+            int D = int.Parse(Console.ReadLine());
+            Console.WriteLine("Syötä 3.luku");
+            int P = int.Parse(Console.ReadLine());
         }
     }
 }
